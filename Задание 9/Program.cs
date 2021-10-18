ac1604ff466f0cf573419b9f2492c8ed95f67725
Анна Белозерова <bel.denisovna@gmail.com>
2021-10-18 10:21:36 +0300

    Добавьте файлы проекта.

---
+++ Задание 9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_9
+{   //Обработка исключений для калькулятора
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("КАЛЬКУЛЯТОР");
+            try
+            {                
+                Console.Write("Введите целое число: Х= ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите целое число: Y= ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Виды возможных операций:\n\t1 - сложение\n\t2 - вычитание\n\t3 - произведение\n\t4 - частное\nВведите код операции:");
+                byte n = Convert.ToByte(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Ваш результат = {0}", x + y);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ваш результат = {0}", x - y);
+                        break;
+                    case 3:
+                        Console.WriteLine("Ваш результат = {0}", x * y);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ваш результат = {0}", x / y);
+                        break;
+                    default:
+                        Console.WriteLine("Выбрана несуществующая операция.");
+                        break;
+                }              
+
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
